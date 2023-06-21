60fa748c5871e35cdc92fce9d0384ef3c292a3b6
Hyeonjong <nianelo4@gmail.com>
2023-06-21 23:52:12 +0900

    feat: define plugin printers
    All but the core part of the print function was copied from https://github.com/ony3000/prettier-plugin-brace-style/blob/v0.1.0/src/printers.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import type { Plugin } from 'prettier';
 
 import { parsers } from './parsers';
+import { printers } from './printers';
 
 const mergePlugin: Plugin = {
   parsers,
+  printers,
 };
 
 export default mergePlugin;
---
+++ src/printers.ts
@@ -0,0 +1,165 @@
+import type { AstPath, ParserOptions, Doc, Printer } from 'prettier';
+
+import { extractPrinter, makeCommentContext } from './utils';
+
+const printerErrorMessage =
+  'There is no default printer or the function to look for does not exist.';
+
+let defaultPrinter: Printer | undefined;
+
+function printWithMergedPlugin(
+  path: AstPath,
+  options: ParserOptions,
+  print: (path: AstPath) => Doc,
+): Doc {
+  const node = path.getValue();
+
+  if (!defaultPrinter && node) {
+    if (
+      (options.parser === 'babel' && node.type === 'File') ||
+      (options.parser === 'typescript' && node.type === 'Program')
+    ) {
+      defaultPrinter = extractPrinter(options);
+    }
+  }
+
+  if (!defaultPrinter) {
+    throw new Error('Default printer does not exist.');
+  }
+
+  const defaultDoc = defaultPrinter.print(path, options, print);
+
+  return defaultDoc;
+}
+
+function canAttachComment(node: any): boolean {
+  return node.type && node.type !== 'comment';
+}
+
+function isBlockCommentWrapper(node: any): boolean {
+  if (defaultPrinter?.isBlockComment) {
+    return defaultPrinter.isBlockComment(node);
+  }
+
+  throw new Error(`[isBlockCommentWrapper] ${printerErrorMessage}`);
+}
+
+function printCommentWrapper(commentPath: AstPath, options: ParserOptions): Doc {
+  if (defaultPrinter?.printComment) {
+    return defaultPrinter.printComment(commentPath, options);
+  }
+
+  throw new Error(`[printCommentWrapper] ${printerErrorMessage}`);
+}
+
+function ownLineWrapper(
+  commentNode: any,
+  text: string,
+  options: ParserOptions,
+  ast: any,
+  isLastComment: boolean,
+): boolean {
+  if (!defaultPrinter && ast) {
+    if (
+      (options.parser === 'babel' && ast.type === 'File') ||
+      (options.parser === 'typescript' && ast.type === 'Program')
+    ) {
+      defaultPrinter = extractPrinter(options);
+    }
+  }
+
+  if (defaultPrinter?.handleComments?.ownLine) {
+    const commentContext = makeCommentContext(commentNode, text, options, ast, isLastComment);
+
+    return defaultPrinter.handleComments.ownLine(commentContext, text, options, ast, isLastComment);
+  }
+
+  throw new Error(`[ownLineWrapper] ${printerErrorMessage}`);
+}
+
+function endOfLineWrapper(
+  commentNode: any,
+  text: string,
+  options: ParserOptions,
+  ast: any,
+  isLastComment: boolean,
+): boolean {
+  if (!defaultPrinter && ast) {
+    if (
+      (options.parser === 'babel' && ast.type === 'File') ||
+      (options.parser === 'typescript' && ast.type === 'Program')
+    ) {
+      defaultPrinter = extractPrinter(options);
+    }
+  }
+
+  if (defaultPrinter?.handleComments?.endOfLine) {
+    const commentContext = makeCommentContext(commentNode, text, options, ast, isLastComment);
+
+    return defaultPrinter.handleComments.endOfLine(
+      commentContext,
+      text,
+      options,
+      ast,
+      isLastComment,
+    );
+  }
+
+  throw new Error(`[endOfLineWrapper] ${printerErrorMessage}`);
+}
+
+function remainingWrapper(
+  commentNode: any,
+  text: string,
+  options: ParserOptions,
+  ast: any,
+  isLastComment: boolean,
+): boolean {
+  if (!defaultPrinter && ast) {
+    if (
+      (options.parser === 'babel' && ast.type === 'File') ||
+      (options.parser === 'typescript' && ast.type === 'Program')
+    ) {
+      defaultPrinter = extractPrinter(options);
+    }
+  }
+
+  if (defaultPrinter?.handleComments?.remaining) {
+    const commentContext = makeCommentContext(commentNode, text, options, ast, isLastComment);
+
+    return defaultPrinter.handleComments.remaining(
+      commentContext,
+      text,
+      options,
+      ast,
+      isLastComment,
+    );
+  }
+
+  throw new Error(`[remainingWrapper] ${printerErrorMessage}`);
+}
+
+export const printers: { [astFormat: string]: Printer } = {
+  'merging-babel-ast': {
+    print: printWithMergedPlugin,
+    canAttachComment,
+    isBlockComment: isBlockCommentWrapper,
+    printComment: printCommentWrapper,
+    handleComments: {
+      ownLine: ownLineWrapper,
+      endOfLine: endOfLineWrapper,
+      remaining: remainingWrapper,
+    },
+  },
+  'merging-typescript-ast': {
+    print: printWithMergedPlugin,
+    canAttachComment,
+    isBlockComment: isBlockCommentWrapper,
+    printComment: printCommentWrapper,
+    handleComments: {
+      ownLine: ownLineWrapper,
+      endOfLine: endOfLineWrapper,
+      remaining: remainingWrapper,
+    },
+  },
+};
