59324d9d88141753efa920e1c18b9f3cb0dee175
Hyeonjong <nianelo4@gmail.com>
2023-06-28 23:59:34 +0900

    build: update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
@@ -17,4 +18,9 @@ export default defineConfig({
       },
     },
   },
+  plugins: [
+    dts({
+      include: ['src/**'],
+    }),
+  ],
 });
