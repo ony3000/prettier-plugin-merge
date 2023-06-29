521390dc99fed84254c389ddfe942e60ac544e9f
Hyeonjong <nianelo4@gmail.com>
2023-06-30 00:36:40 +0900

    test: update plugin-less tests

--- tests/babel/plugin-less/expected-results.ts
+++ tests/babel/plugin-less/expected-results.ts
@@ -19,3 +19,44 @@ export default function Counter({ label = "Counter", onChange = undefined }) {
   );
 }
 `;
+
+export const jestConfigCodeResult = `const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+
+  testEnvironment: "jest-environment-jsdom",
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
+`;
+
+export const jestSetupCodeResult = `// Optional: configure or set up a testing framework before each test.
+// If you delete this file, remove \`setupFilesAfterEnv\` from \`jest.config.js\`
+
+// Used for __tests__/testing-library.js
+// Learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom/extend-expect";
+`;
+
+export const nextConfigCodeResult = `/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: "standalone",
+};
+
+module.exports = nextConfig;
+`;
--- tests/typescript/plugin-less/index.test.ts
+++ tests/typescript/plugin-less/index.test.ts
@@ -3,29 +3,75 @@ import type { Options } from 'prettier';
 import mergePlugin from '@/index';
 
 import { format } from '../../settings';
-import { counterComponentCode } from '../fixtures';
-import { counterComponentCodeResult } from './expected-results';
+import { counterComponentCode, nextEnvDTSCode, nextAPIRouteCode } from '../fixtures';
+import {
+  counterComponentCodeResult,
+  nextEnvDTSCodeResult,
+  nextAPIRouteCodeResult,
+} from './expected-results';
 
 const baseOptions: Options = {
   parser: 'typescript',
 };
 
 describe('[typescript] plugin-less', () => {
-  test('No plugin', () => {
-    const options = {
-      ...baseOptions,
-      plugins: [],
-    };
+  describe('Counter Component', () => {
+    test('No plugin', () => {
+      const options = {
+        ...baseOptions,
+        plugins: [],
+      };
 
-    expect(format(counterComponentCode, options)).toBe(counterComponentCodeResult);
+      expect(format(counterComponentCode, options)).toBe(counterComponentCodeResult);
+    });
+
+    test('Merge plugin alone has no effect', () => {
+      const options = {
+        ...baseOptions,
+        plugins: [mergePlugin],
+      };
+
+      expect(format(counterComponentCode, options)).toBe(counterComponentCodeResult);
+    });
   });
 
-  test('Merge plugin alone has no effect', () => {
-    const options = {
-      ...baseOptions,
-      plugins: [mergePlugin],
-    };
+  describe('next-env.d.ts', () => {
+    test('No plugin', () => {
+      const options = {
+        ...baseOptions,
+        plugins: [],
+      };
 
-    expect(format(counterComponentCode, options)).toBe(counterComponentCodeResult);
+      expect(format(nextEnvDTSCode, options)).toBe(nextEnvDTSCodeResult);
+    });
+
+    test('Merge plugin alone has no effect', () => {
+      const options = {
+        ...baseOptions,
+        plugins: [mergePlugin],
+      };
+
+      expect(format(nextEnvDTSCode, options)).toBe(nextEnvDTSCodeResult);
+    });
+  });
+
+  describe('Next.js API route', () => {
+    test('No plugin', () => {
+      const options = {
+        ...baseOptions,
+        plugins: [],
+      };
+
+      expect(format(nextAPIRouteCode, options)).toBe(nextAPIRouteCodeResult);
+    });
+
+    test('Merge plugin alone has no effect', () => {
+      const options = {
+        ...baseOptions,
+        plugins: [mergePlugin],
+      };
+
+      expect(format(nextAPIRouteCode, options)).toBe(nextAPIRouteCodeResult);
+    });
   });
 });
