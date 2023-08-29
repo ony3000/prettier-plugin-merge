46c7cf1b50216bb8a5f7ee6d151277472ce3637a
Hyeonjong <nianelo4@gmail.com>
2023-08-29 21:45:06 +0900

    test: add tests
    This test checks the situation where this plugin's print function is called even if another plugin exists after this plugin.

---
+++ tests/typescript/dummy-plugin.test.ts
@@ -0,0 +1,39 @@
+import type { Options, Plugin } from 'prettier';
+
+import mergePlugin from '@/index';
+
+import { format, dummyPlugin } from '../settings';
+
+let callCount = 0;
+
+const printerWrappedMergePlugin: Plugin = {
+  ...mergePlugin,
+  printers: {
+    'merging-ast': {
+      print: (...args) => {
+        callCount += 1;
+
+        if (callCount > 10) {
+          throw new Error('Aborting formatting because it may have caused an infinite loop.');
+        }
+
+        return mergePlugin.printers!['merging-ast'].print(...args);
+      },
+    },
+  },
+};
+
+const options: Options = {
+  parser: 'typescript',
+  plugins: [printerWrappedMergePlugin, dummyPlugin],
+};
+
+describe('typescript/dummy-plugin', () => {
+  test('print function should only be called once', () => {
+    const input = '// Lorem ipsum\n';
+
+    callCount = 0;
+    format(input, options);
+    expect(callCount).toBe(1);
+  });
+});
