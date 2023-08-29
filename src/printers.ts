2de2af5fce610ebc4e61ffd75a8b135cd98ddc38
Hyeonjong <nianelo4@gmail.com>
2023-08-29 22:56:07 +0900

    feat: implements sequential formatting
    for plugins up to immediately preceding this plugin.

--- src/printers.ts
+++ src/printers.ts
@@ -11,7 +11,22 @@ function printWithMergedPlugin(
 
   const { originalText } = options;
   const plugins = options.plugins.filter((plugin) => typeof plugin !== 'string') as Plugin[];
-  const sequentiallyFormattedText = plugins.slice(0, -1).reduce(
+  const pluginIndex = plugins.findIndex(
+    (plugin) =>
+      Object.values(plugin.parsers ?? {}).every((parser) => parser.astFormat === 'merging-ast') &&
+      Object.entries(plugin.printers ?? {}).every(
+        ([astFormat, printer]) =>
+          astFormat === 'merging-ast' && Object.keys(printer).every((key) => key === 'print'),
+      ),
+  );
+
+  if (pluginIndex === -1) {
+    throw new Error(
+      "The structure of this plugin may have changed. If it's not in development, you may need to report an issue.",
+    );
+  }
+
+  const sequentiallyFormattedText = plugins.slice(0, pluginIndex).reduce(
     (previousText, plugin) =>
       format(previousText, {
         ...options,
