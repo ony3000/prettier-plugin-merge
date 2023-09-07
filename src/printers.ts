74309d049006c9fc283d328b246f995d9dc73dea
Hyeonjong <nianelo4@gmail.com>
2023-09-07 21:53:06 +0900

    refactor: make print function as closure

--- src/printers.ts
+++ src/printers.ts
@@ -1,53 +1,57 @@
 import type { AstPath, ParserOptions, Doc, Printer, Plugin } from 'prettier';
 import { format } from 'prettier';
 
-function printWithMergedPlugin(
-  path: AstPath,
-  options: ParserOptions,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  print: (path: AstPath) => Doc,
-): Doc {
-  const node = path.getValue();
+function createPrinter(): Printer {
+  function main(
+    path: AstPath,
+    options: ParserOptions,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    print: (path: AstPath) => Doc,
+  ): Doc {
+    const node = path.getValue();
 
-  const { originalText } = options;
-  const plugins = options.plugins.filter((plugin) => typeof plugin !== 'string') as Plugin[];
-  const pluginIndex = plugins.findIndex(
-    (plugin) =>
-      Object.values(plugin.parsers ?? {}).every((parser) => parser.astFormat === 'merging-ast') &&
-      Object.entries(plugin.printers ?? {}).every(
-        ([astFormat, printer]) =>
-          astFormat === 'merging-ast' && Object.keys(printer).every((key) => key === 'print'),
-      ),
-  );
-
-  if (pluginIndex === -1) {
-    throw new Error(
-      "The structure of this plugin may have changed. If it's not in development, you may need to report an issue.",
+    const { originalText } = options;
+    const plugins = options.plugins.filter((plugin) => typeof plugin !== 'string') as Plugin[];
+    const pluginIndex = plugins.findIndex(
+      (plugin) =>
+        Object.values(plugin.parsers ?? {}).every((parser) => parser.astFormat === 'merging-ast') &&
+        Object.entries(plugin.printers ?? {}).every(
+          ([astFormat, printer]) =>
+            astFormat === 'merging-ast' && Object.keys(printer).every((key) => key === 'print'),
+        ),
     );
+
+    if (pluginIndex === -1) {
+      throw new Error(
+        "The structure of this plugin may have changed. If it's not in development, you may need to report an issue.",
+      );
+    }
+
+    const sequentiallyFormattedText = plugins.slice(0, pluginIndex).reduce(
+      (previousText, plugin) =>
+        format(previousText, {
+          ...options,
+          plugins: [plugin],
+          rangeEnd: Infinity,
+        }),
+      originalText,
+    );
+
+    if (node?.comments) {
+      node.comments.forEach((comment: any) => {
+        // eslint-disable-next-line no-param-reassign
+        comment.printed = true;
+      });
+    }
+
+    return sequentiallyFormattedText;
   }
 
-  const sequentiallyFormattedText = plugins.slice(0, pluginIndex).reduce(
-    (previousText, plugin) =>
-      format(previousText, {
-        ...options,
-        plugins: [plugin],
-        rangeEnd: Infinity,
-      }),
-    originalText,
-  );
-
-  if (node?.comments) {
-    node.comments.forEach((comment: any) => {
-      // eslint-disable-next-line no-param-reassign
-      comment.printed = true;
-    });
-  }
-
-  return sequentiallyFormattedText;
+  return {
+    print: main,
+  };
 }
 
 export const printers: { [astFormat: string]: Printer } = {
-  'merging-ast': {
-    print: printWithMergedPlugin,
-  },
+  'merging-ast': createPrinter(),
 };
