b3c1d1e2bc7ac9274f0474b0983d99e3d6a5aa73
Hyeonjong <nianelo4@gmail.com>
2023-09-28 23:17:01 +0900

    chore: create test-suite workspace

--- packages/test-suite/jest.config.cjs
+++ packages/test-suite/jest.config.cjs
@@ -1,8 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
   testEnvironment: 'node',
 };
