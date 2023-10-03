711522e1ef524838d98116832e8fe6810fc996f7
Hyeonjong <nianelo4@gmail.com>
2023-10-04 00:49:06 +0900

    test: convert multiple plugin tests into workspace

---
+++ tests/v2-using-multiple-compatible-plugins/prettier.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  // explicit default options
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxBracketSameLine: false,
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: "preserve",
+  arrowParens: "always",
+  htmlWhitespaceSensitivity: "css",
+  endOfLine: "lf",
+  quoteProps: "as-needed",
+  vueIndentScriptAndStyle: false,
+  embeddedLanguageFormatting: "auto",
+  singleAttributePerLine: false,
+
+  // plugins and plugin options
+  plugins: [
+    "prettier-plugin-brace-style",
+    "prettier-plugin-tailwindcss",
+    "bundle-entry",
+  ],
+  overrides: [
+    {
+      files: ["babel/**", "typescript/**"],
+      options: {
+        braceStyle: "allman",
+      },
+    },
+  ],
+};
---
+++ tests/v2-using-multiple-plugins-with-some-v3-plugin/index.test.ts
@@ -0,0 +1,30 @@
+import { execSync } from "child_process";
+import { resolve, sep } from "path";
+
+const entryPoints = ["babel/Counter.jsx", "typescript/Counter.tsx"];
+
+describe("prettier v2 test using multiple plugins (some of which are only available for prettier v3)", () => {
+  for (const entry of entryPoints) {
+    const filePath = resolve(__dirname, entry).split(sep).join("/");
+    const command = `pnpm prettier --check ${filePath}`;
+
+    test(entry, () => {
+      let isFormatted = true;
+
+      try {
+        execSync(command);
+      } catch (error) {
+        if (
+          // @ts-ignore
+          error?.message?.match(/require\(\) of ES Module [^ ]+ not supported/)
+        ) {
+          // In this case, it is treated as if the plugin does not exist.
+        } else {
+          isFormatted = false;
+        }
+      }
+
+      expect(isFormatted).toBe(true);
+    });
+  }
+});
