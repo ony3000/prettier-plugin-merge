21805ce69a8ad1abafb47923742efc300d2fd8eb
Hyeonjong <nianelo4@gmail.com>
2023-10-03 21:52:55 +0900

    chore: fix broken tests

--- packages/bundle-entry/standalone.ts
+++ packages/bundle-entry/standalone.ts
@@ -1,3 +1,8 @@
-import v2Plugin from 'v2-plugin';
+import { parsers as v2Parsers, printers as v2Printers } from 'v2-plugin';
+
+const v2Plugin = {
+  parsers: v2Parsers,
+  printers: v2Printers,
+};
 
 export { v2Plugin };
