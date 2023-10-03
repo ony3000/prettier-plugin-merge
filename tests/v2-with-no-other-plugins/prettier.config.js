9bfea6e894dcb85013da628bd13811c3389793ce
Hyeonjong <nianelo4@gmail.com>
2023-10-03 12:09:58 +0900

    test: convert plugin-less tests into workspace

---
+++ tests/v2-with-no-other-plugins/prettier.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  // explicit default options
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxBracketSameLine: false,
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: "preserve",
+  arrowParens: "always",
+  htmlWhitespaceSensitivity: "css",
+  endOfLine: "lf",
+  quoteProps: "as-needed",
+  vueIndentScriptAndStyle: false,
+  embeddedLanguageFormatting: "auto",
+  singleAttributePerLine: false,
+
+  // plugins and plugin options
+  plugins: ["bundle-entry"],
+};
