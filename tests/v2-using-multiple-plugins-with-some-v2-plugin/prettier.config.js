79ba6a2967656a6d7a9a8671a0e5733dc6ee0cf2
Hyeonjong <nianelo4@gmail.com>
2023-10-04 21:13:08 +0900

    chore: convert explicit default options into workspace

--- tests/v2-using-multiple-plugins-with-some-v2-plugin/prettier.config.js
+++ tests/v2-using-multiple-plugins-with-some-v2-plugin/prettier.config.js
@@ -1,27 +1,7 @@
+const baseConfig = require("prettier-config/base");
+
 module.exports = {
-  // explicit default options
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
-  semi: true,
-  singleQuote: false,
-  jsxSingleQuote: false,
-  trailingComma: "all",
-  bracketSpacing: true,
-  bracketSameLine: false,
-  jsxBracketSameLine: false,
-  rangeStart: 0,
-  rangeEnd: Infinity,
-  requirePragma: false,
-  insertPragma: false,
-  proseWrap: "preserve",
-  arrowParens: "always",
-  htmlWhitespaceSensitivity: "css",
-  endOfLine: "lf",
-  quoteProps: "as-needed",
-  vueIndentScriptAndStyle: false,
-  embeddedLanguageFormatting: "auto",
-  singleAttributePerLine: false,
+  ...baseConfig,
 
   // plugins and plugin options
   plugins: [
