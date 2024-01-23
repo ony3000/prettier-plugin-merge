453f6287d23804ebace581fac32784b231646ad1
Hyeonjong <nianelo4@gmail.com>
2024-01-23 21:28:11 +0900

    chore: remove unnecessary workspaces

--- tests/v2-loop/adaptors/prettier-plugin-incomplete.js
+++
@@ -1,9 +0,0 @@
-const mergePlugin = require("prettier-plugin-merge");
-
-const incompleteMergePlugin = {
-  parsers: {
-    ...mergePlugin.parsers,
-  },
-};
-
-module.exports = incompleteMergePlugin;
