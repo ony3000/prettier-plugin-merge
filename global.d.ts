96820cadf036b5dd882ddaf9a8ffe81612ab913a
Hyeonjong <nianelo4@gmail.com>
2024-06-02 14:38:49 +0900

    chore: remove unused global augmentation

--- global.d.ts
+++ global.d.ts
@@ -1,14 +1,4 @@
-import * as Diff from 'diff';
-
 declare global {
-  namespace Diff {
-    export function applyPatch(
-      source: string,
-      patch: string | Diff.ParsedDiff | [Diff.ParsedDiff],
-      options?: Diff.ApplyPatchOptions,
-    ): string | false;
-  }
-
   type PrettierBaseOptions = {
     printWidth: number;
     tabWidth: number;
